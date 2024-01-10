6462ba404e23f626bba4a979a87ec9aefe50cd8c
Timothy Machin <128641054+tmachin-afit@users.noreply.github.com>
2024-01-09 21:44:54 -0500

    Update lab2Setup.sh

--- lab2Setup.sh
+++ lab2Setup.sh
@@ -1,9 +1,7 @@
 # ensure updated system
-sudo apt update && sudo apt upgrade -y
-sudo snap refresh
-sudo snap install --classic code
+apt update && sudo apt upgrade -y
 # install pip (python package manager) if not installed already
-sudo apt install -y python3-pip
+apt install -y python3-pip
 python3 -m pip install --upgrade pip
 # install python requirements
 python3 -m pip install -r requirements.txt
