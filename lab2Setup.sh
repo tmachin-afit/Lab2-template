a639997fe33268d4865c57f8764114aba4414d0f
Timothy Machin <128641054+tmachin-afit@users.noreply.github.com>
2024-04-05 03:36:07 -0400

    Update lab2Setup.sh

--- lab2Setup.sh
+++ lab2Setup.sh
@@ -1,5 +1,5 @@
 # ensure updated system
-apt update && sudo apt upgrade -y
+apt update && apt upgrade -y
 # install pip (python package manager) if not installed already
 apt install -y python3-pip
 python3 -m pip install --upgrade pip
